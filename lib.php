bc630393d46aa8ff07eb059f345aab395a230c30
Thomas Winkler <thomas.winkler@wunderbyte.at>
2022-06-30 11:38:43 +0200

    modulename 0

--- lib.php
+++ lib.php
@@ -567,8 +567,8 @@ function zoom_get_monthweek_options() {
 function zoom_populate_calender_item(stdClass $zoom, stdClass $occurrence = null) {
     $event = new stdClass();
     $event->type = CALENDAR_EVENT_TYPE_STANDARD;
-    $event->modulename = 'zoom';
-    $event->eventtype = 'site';
+    $event->modulename = 0;
+    $event->eventtype = 'zoom';
     $event->courseid = SITEID;
     $event->categoryid = 0;
     /* legacy
